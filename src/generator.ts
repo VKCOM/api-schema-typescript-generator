59a89dc78e8d94001e224aeb783347ac49841a32
Igor Fedorov <iamfedoroff@yandex.ru>
2021-04-04 12:25:23 +0300

    refactor: Move generateImportsBlock to generator.ts

--- src/generator.ts
+++ src/generator.ts
@@ -1,9 +1,45 @@
 import { SchemaObject } from './generators/SchemaObject';
 import { CodeBlocksArray, GeneratorResultInterface } from './generators/BaseCodeBlock';
-import { getEnumPropertyName, getInterfaceName, joinOneOfValues } from './helpers';
+import { getEnumPropertyName, getInterfaceName, getSectionFromObjectName, joinOneOfValues } from './helpers';
 import { TypeCodeBlock, TypeScriptCodeTypes } from './generators/TypeCodeBlock';
-import { quoteJavaScriptValue } from './utils';
+import { quoteJavaScriptValue, sortArrayAlphabetically, uniqueArray } from './utils';
 import { newLineChar } from './constants';
+import { Dictionary, ObjectType, RefsDictionary } from './types';
+
+export function generateImportsBlock(imports: RefsDictionary, section: string | null, type?: ObjectType): string {
+  const objectsToImport = uniqueArray(Object.keys(imports));
+  const paths: Dictionary<string[]> = {};
+
+  objectsToImport.forEach((objectName) => {
+    const importSection = getSectionFromObjectName(objectName);
+    const interfaceName = getInterfaceName(objectName);
+    let path;
+
+    if (type === ObjectType.Object) {
+      if (section === importSection) {
+        path = `./${interfaceName}`;
+      } else {
+        path = `../${importSection}/${interfaceName}`;
+      }
+    } else {
+      path = `../objects/${importSection}/${interfaceName}`;
+    }
+
+    if (!paths[path]) {
+      paths[path] = [];
+    }
+    paths[path].push(interfaceName);
+  });
+
+  const importLines: string[] = [];
+
+  sortArrayAlphabetically(Object.keys(paths)).forEach((path) => {
+    const interfaces = sortArrayAlphabetically(paths[path]).join(', ');
+    importLines.push(`import { ${interfaces} } from '${path}';`);
+  });
+
+  return importLines.join(newLineChar);
+}
 
 interface GenerateInlineEnumOptions {
   objectParentName?: string;
